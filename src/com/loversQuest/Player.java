66b8404b63c543dd26073b949ac3deb85f23bbf4
jai515 <jai.515@gmail.com>
2020-08-27 16:55:55 -0700

    file restructure

---
+++ src/com/loversQuest/Player.java
@@ -0,0 +1,48 @@
+package com.loversQuest;
+
+public class Player {
+
+    private String name;
+    private Location currentLocation;
+
+    // CTOR
+    public Player(String name, Location currentLocation) {
+        this.name = name;
+        this.currentLocation = currentLocation;
+    }
+
+    // BUSINESS METHODS
+    public void go(String directionInput) {
+
+        String direction = directionInput.toLowerCase();
+
+        if (direction.equals("east")) {
+            this.setCurrentLocation(this.currentLocation.getEast());
+        } else if (direction.equals("west")) {
+            this.setCurrentLocation(this.currentLocation.getWest());
+        } else if (direction.equals("south")) {
+            this.setCurrentLocation(this.currentLocation.getSouth());
+        } else if (direction.equals("north")){
+            this.setCurrentLocation(this.currentLocation.getNorth());
+        } else {
+            System.out.println("bad input, try again");
+        }
+    }
+
+    // SETTERS/GETTERS
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Location getCurrentLocation() {
+        return currentLocation;
+    }
+
+    public void setCurrentLocation(Location currentLocation) {
+        this.currentLocation = currentLocation;
+    }
+}

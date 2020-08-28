4e470a0099fe2d0e433d26cae9988b0d44c9d6a6
jai515 <jai.515@gmail.com>
2020-08-28 13:43:40 -0700

    initial game loop

--- src/GameInterface.java
+++ src/GameInterface.java
@@ -1,16 +1,19 @@
 import com.loversQuest.*;
-import com.loversQuest.IO.Output;
-import com.loversQuest.gameWorld.Player;
+import com.loversQuest.IO.*;
 
 public class GameInterface {
 
     public static void main(String[] args) {
-        Game g1 = new Game();
+        GameWorld g1 = new GameWorld();
+        Input input = new Input(g1);
         g1.createMap();
-
         Output.displayIntroDialog();
-        System.out.println(g1.p1.getCurrentLocation());
-        g1.p1.go("east");
-        System.out.println(g1.p1.getCurrentLocation());
+        System.out.println(g1.px);;
+
+        while(!g1.p1.getCurrentLocation().getName().equals("GAZEBO")){
+            System.out.println(g1.p1.getCurrentLocation());
+            input.userActionPrompt();
+        }
+
     }
 }

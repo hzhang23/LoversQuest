e17ccc8f708543e4e1c3bcffe847fbba3343839f
Luke Thompson <coachluke@gmail.com>
2020-09-21 15:48:55 -0700

    corrections to excel files

--- src/com/loversQuest/GameInit.java
+++ src/com/loversQuest/GameInit.java
@@ -6,12 +6,7 @@ import java.util.Map;
 
 public class GameInit {
     public static Map<String, Location> locationMap = ReadExcel.getLocationMap();
-    public Player p1 = new Player("Bob", locationMap.get("gazebo"));
-
-    public void addRing(){
-        p1.addItem(new Item("Diamond Ring", "test ring"));
-    }
-
+    public Player p1 = new Player("Bob", locationMap.get("barracks"));
     public boolean isGameOver(){
         boolean gameOver = false;
         if(this.p1.isHasKiss()){
